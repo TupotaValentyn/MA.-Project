84e725e4e320e5230139abad1c5e5aba4bbc7ca0
Dima <v3il@ukr.net>
2020-03-12 13:25:28 +0200

    readme update

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -2,12 +2,10 @@ import { Router } from 'express';
 
 import authRouter from './auth';
 import mainRouter from './home';
-import errorsHandler from '../errorsHandler';
 
 const router = Router();
 
 router.use('/', mainRouter);
 router.use('/login', authRouter);
-router.use(errorsHandler);
 
 export default router;
