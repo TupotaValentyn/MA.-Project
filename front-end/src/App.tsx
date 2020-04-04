f6c124d631dd0194f3d2c6dc29faf258c7864c69
TupotaValentyn <gm.tupota.valik@gmail.com>
2020-04-04 14:58:08 +0300

    Add lint, prittier, packages, tsconfig, editorconfig

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react';
 import './App.css';
+import { Button } from '@material-ui/core';
 
-function App() {
+type Props = {};
+
+const App: FC<Props> = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button color="primary" variant="contained">
+        text
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
