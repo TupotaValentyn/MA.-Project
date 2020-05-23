3538075ae99e55d5225ee534f9e8964c1f3c509f
Dima <v3il@ukr.net>
2020-05-23 16:29:44 +0300

    Added doc for password reset API

--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -62,8 +62,60 @@ router.get('/:token', usersController.getUserByToken);
  */
 router.post('/change_locale', authorized, usersController.changeLocale);
 
+/**
+ * @swagger
+ * /user/reset_password:
+ *    post:
+ *      tags:
+ *        - User
+ *      summary: "Позволяет пользователю сменить пароль аккаунта"
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - in: "body"
+ *          name: "email"
+ *          required: true
+ *          description: "Почта пользователя. На нее будет отправлена ссылка для сброса пароля"
+ *          schema:
+ *            type: string
+ *      responses:
+ *        '200':
+ *          description: "Письмо успешно отправлено. Возвращает ответ вида ```{ sent: boolean }```."
+ *        '400':
+ *          description: Неправильный запрос. Не передан email или пользователя с таким email не существует.
+ *
+ */
 router.post('/reset_password', usersController.resetPassword);
 
+/**
+ * @swagger
+ * /user/update_password:
+ *    post:
+ *      tags:
+ *        - User
+ *      summary: "Позволяет пользователю заменить пароль аккаунта"
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - in: "body"
+ *          name: "userHash"
+ *          required: true
+ *          description: "Хеш пользователя. Нужно взять из URL страницы ввода нового пароля"
+ *          schema:
+ *            type: string
+ *        - in: "body"
+ *          name: "password"
+ *          required: true
+ *          description: "Новый пароль аккаунта (мин. 8 символов)"
+ *          schema:
+ *            type: string
+ *      responses:
+ *        '200':
+ *          description: "Пароль успешно изменен. Возвращает ответ вида ```{ updated: boolean }```."
+ *        '400':
+ *          description: Неправильный запрос. Не переданы параметры, некорректный пароль или пользователя с таким userHash не существует.
+ *
+ */
 router.post('/update_password', usersController.updatePassword);
 
 export default router;
